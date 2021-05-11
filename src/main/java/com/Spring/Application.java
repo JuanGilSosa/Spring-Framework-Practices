bc75568373867f26a1bfafe12730a43ff96a7ccd
Juan Gil Sosa <juanmanuels499@gmail.com>
2021-05-11 10:45:38 -0300

    .

--- src/main/java/com/Spring/Application.java
+++ src/main/java/com/Spring/Application.java
@@ -2,17 +2,13 @@ package com.Spring;
 
 import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.swing.*;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(Application.class, args);
 		try{
 			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("service.xml");
 			com.Models.Automovil fiat = context.getBean("miAuto", com.Models.Automovil.class);
